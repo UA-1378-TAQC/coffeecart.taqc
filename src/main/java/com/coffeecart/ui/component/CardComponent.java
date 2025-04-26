b49c6d216143af9e22d7a19489ee6e2b87202f66
Roman <romankmet10@gmail.com>
2025-04-26 17:36:15 +0300

    Card Component first implementation

--- src/main/java/com/coffeecart/ui/component/CardComponent.java
+++ src/main/java/com/coffeecart/ui/component/CardComponent.java
@@ -3,7 +3,12 @@ package com.coffeecart.ui.component;
 import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class CardComponent extends BaseComponent {
@@ -16,8 +21,19 @@ public class CardComponent extends BaseComponent {
     @Getter
     @FindBy(xpath = ".//div[contains(@class, 'cup')]")
     private WebElement cupRootElement;
+    @Getter
+    @FindBy(xpath = "//div[contains(@class, 'confirmation-dialog')]")
+    private WebElement confirmationDialog;
+    @Getter
+    @FindBy(xpath = "//button[contains(text(), 'Yes')]")
+    private WebElement confirmYesButton;
+    @Getter
+    @FindBy(xpath = "//button[contains(text(), 'No')]")
+    private WebElement confirmNoButton;
 
     private CupComponent cupComponent;
+
+    private static final Duration TIMEOUT = Duration.ofSeconds(10);
 
     public CardComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
@@ -36,5 +52,27 @@ public class CardComponent extends BaseComponent {
     public CupComponent getCupComponent() {
         return cupComponent;
     }
-}
 
+    public void addToCartViaLeftClick() {
+        rootElement.click();
+    }
+
+    public void addToCartViaContextMenu() {
+        Actions actions = new Actions(driver);
+        actions.contextClick(rootElement).perform();
+
+        WebElement dialog = waitForElementToBeVisible(confirmationDialog);
+        confirmYesButton.click();
+    }
+    public void rejectAddToCartViaContextMenu() {
+        Actions actions = new Actions(driver);
+        actions.contextClick(rootElement).perform();
+
+        WebElement dialog = waitForElementToBeVisible(confirmationDialog);
+        confirmNoButton.click();
+    }
+    private WebElement waitForElementToBeVisible(WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
