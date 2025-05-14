d585b709e240f3ff82e92fa09d75d4f15615e6de
Roman <romankmet10@gmail.com>
2025-05-14 15:53:39 +0300

    Added the DISCOUNTED_MOCHA to the DrinkEnum Scoped the DRINK1, DRINK2, and DRINK3 variables inside the test method Replaced the softAssert with a regular assertTrue

--- src/test/java/com/coffeecart/ui/TestDiscountedMochaButton.java
+++ src/test/java/com/coffeecart/ui/TestDiscountedMochaButton.java
@@ -10,16 +10,10 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Issue;
 import io.qameta.allure.Owner;
-import org.testng.asserts.SoftAssert;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class TestDiscountedMochaButton extends BaseTestRunner {
-
-    private static final String DISCOUNTED_MOCHA = "(Discounted) Mocha";
-
-    private static final String DRINK1 = DrinkEnum.ESPRESSO.getRecipe().getName();
-    private static final String DRINK2 = DrinkEnum.ESPRESSO_MACCHIATO.getRecipe().getName();
-    private static final String DRINK3 = DrinkEnum.CAPPUCCINO.getRecipe().getName();
 
     @Test
     @Description("Verify '+' button is disabled for '(Discounted) Mocha' in the Cart Page")
@@ -27,31 +21,30 @@ public class TestDiscountedMochaButton extends BaseTestRunner {
     @Issue("19")
     @Owner("Roman Kmet")
     public void testPlusButtonForDiscountedMocha() {
-        SoftAssert softAssert = new SoftAssert();
         MenuPage menuPage = new MenuPage(driver);
 
-        menuPage.clickDrink(DRINK1).clickDrink(DRINK2).clickDrink(DRINK3);
+        menuPage.clickDrink(DrinkEnum.ESPRESSO.getRecipe().getName())
+                .clickDrink(DrinkEnum.ESPRESSO_MACCHIATO.getRecipe().getName())
+                .clickDrink(DrinkEnum.CAPPUCCINO.getRecipe().getName());
 
         LuckyDayComponent luckyDayComponent = menuPage.getLuckyDayComponent();
-        softAssert.assertTrue(menuPage.getLuckyDayModalRoot().isDisplayed(), "The LuckyDayModal should be displayed.");
+        assertTrue(menuPage.getLuckyDayModalRoot().isDisplayed(), "The LuckyDayModal should be displayed.");
         luckyDayComponent.clickYes();
 
         CartPage cartPage = menuPage.goToCartPage();
         
-        FullItemComponent mochaItem = cartPage.getFullItemByName(DISCOUNTED_MOCHA);
-        softAssert.assertNotNull(mochaItem, "Discounted Mocha should be present in cart");
-        softAssert.assertEquals(mochaItem.getCount(), 1,
+        FullItemComponent mochaItem = cartPage.getFullItemByName(DrinkEnum.DISCOUNTED_MOCHA.getRecipe().getName());
+        assertNotNull(mochaItem, "Discounted Mocha should be present in cart");
+        assertEquals(mochaItem.getCount(), 1,
                 String.format("Mocha quantity should be 1 but was %d", mochaItem.getCount()));
 
         mochaItem.clickOnAddButton();
 
         int updatedCount = mochaItem.getCount();
-        softAssert.assertEquals(updatedCount, 1, 
+        assertEquals(updatedCount, 1,
                 String.format("Mocha quantity should remain 1 but was %d after clicking '+' button", updatedCount));
 
         boolean isPlusButtonEnabled = mochaItem.plusButtonIsEnabled();
-        softAssert.assertFalse(isPlusButtonEnabled, "The '+' button should be disabled when Mocha quantity is 1");
-
-        softAssert.assertAll();
+        assertFalse(isPlusButtonEnabled, "The '+' button should be disabled when Mocha quantity is 1");
     }
 }
